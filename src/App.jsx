90c94670705bd6d7aa47de993e3a8aa6cb929fff
mukes <mukeshkumarsingh.4462@gmail.com>
2024-01-16 19:57:57 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './../components/logo.png'
+import logo_black from './../components/logo_black.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <img src={logo} alt="" className='myImg' />
+    </div>
+  );
+}
+
+export default App
